675b3afed269bf8aeb8e9ed39c22f98a355130ce
pc <z1top133@ajou.ac.kr>
2020-05-05 00:14:40 +0900

    The Sixth test.

---
+++ eclipse-workspace_2/BowlingGame/src/Game.java
@@ -0,0 +1,42 @@
+
+public class Game {
+	private int[] rolls = new int[21];
+	private int currentRoll = 0;
+
+	public void roll(int pins) {
+		rolls[currentRoll++] = pins;
+	}
+
+	public int score() {
+		int score = 0;
+		int frameIndex = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if (isStrike(frameIndex))// strike
+			{
+				score += 10 + strikeBonus(frameIndex);
+				frameIndex++;
+			} else if (isSpare(frameIndex))// spare
+			{
+				score += 10 + rolls[frameIndex + 2];
+				frameIndex += 2;
+			} else {
+				score += rolls[frameIndex] + rolls[frameIndex + 1];
+				frameIndex += 2;
+			}
+		}
+		return score;
+	}
+
+	private boolean isStrike(int frameIndex) {
+		return rolls[frameIndex] == 10;
+	}
+
+	private int strikeBonus(int frameIndex) {
+		return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+	}
+
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+	}
+
+}
