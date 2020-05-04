5c25bd380a528a6ed6796d0fe63ca28bebd9fc8e
pc <z1top133@ajou.ac.kr>
2020-05-05 00:07:06 +0900

    The Fourth test.

--- eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
+++ eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
@@ -6,36 +6,49 @@ import org.junit.Test;
 
 public class BowlingGameTest {
 	private Game g;
-	
+
 	@Before
 	public void setUp() {
 		g = new Game();
 	}
-	
+
 	private void rollMany(int n, int pins) {
-		for(int i=0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
 			g.roll(pins);
 		}
 	}
-	
+
 	@Test
 	public void testGutterGame() {
 		rollMany(20, 0);
 		assertEquals(0, g.score());
 	}
-	
+
 	@Test
 	public void testAllOnes() {
-		rollMany(20,1);
+		rollMany(20, 1);
 		assertEquals(20, g.score());
 	}
-	
+
 	@Test
 	public void testOneSpare() {
 		rollSpare();
 		g.roll(3);
-		rollMany(17,0);
-		assertEquals(16,g.score());
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
+
+	@Test
+	public void testOneStrike() {
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0);
+		assertEquals(24, g.score());
+	}
+
+	private void rollStrike() {
+		g.roll(10);
 	}
 
 	private void rollSpare() {
