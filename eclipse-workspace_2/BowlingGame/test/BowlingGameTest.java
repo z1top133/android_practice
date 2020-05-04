de21137be689be5f4a91888348872b02f2ebd9fa
pc <z1top133@ajou.ac.kr>
2020-05-04 23:35:06 +0900

    The Second test.

--- eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
+++ eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
@@ -1,16 +1,30 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
-
+	private Game g;
+	
+	@Before
+	public void setUp() {
+		g = new Game();
+	}
+	
 	@Test
 	public void testGutterGame() {
-		Game g = new Game();
 		for(int i=0; i<20; i++) {
 			g.roll(0);
 		}
 		assertEquals(0, g.score());
+	}
+	
+	@Test
+	public void testAllOnes() {
+		for(int i=0; i<20; i++) {
+			g.roll(1);
+		}
+		assertEquals(20, g.score());
 	}
 
 }
