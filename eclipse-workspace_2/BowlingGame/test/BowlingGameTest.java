ebbf91a6fddc508233362fb986104bc3f136933c
pc <z1top133@ajou.ac.kr>
2020-05-04 23:27:28 +0900

    The first test.

---
+++ eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void testGutterGame() {
+		Game g = new Game();
+		for(int i=0; i<20; i++) {
+			g.roll(0);
+		}
+		assertEquals(0, g.score());
+	}
+
+}
---
+++ eclipse-workspace_2/BowlingGame/test/Game.java
@@ -0,0 +1,12 @@
+
+public class Game {
+
+	public void roll(int pins) {
+		
+	}
+
+	public int score() {
+		return 0;
+	}
+
+}
