ba852ea0953eae370074d3bc6b25269694c276fc
pc <z1top133@ajou.ac.kr>
2020-05-04 23:59:11 +0900

    The Third test.

--- eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
+++ eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
@@ -32,11 +32,15 @@ public class BowlingGameTest {
 	
 	@Test
 	public void testOneSpare() {
-		g.roll(5);
-		g.roll(5);
+		rollSpare();
 		g.roll(3);
 		rollMany(17,0);
 		assertEquals(16,g.score());
+	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
 	}
 
 }
--- eclipse-workspace_2/BowlingGame/test/Game.java
+++ eclipse-workspace_2/BowlingGame/test/Game.java
@@ -10,17 +10,21 @@ public class Game {
 
 	public int score() {
 		int score = 0;
-		int i = 0;
+		int frameIndex = 0;
 		for(int frame=0; frame<10; frame++) {
-			if(rolls[i] + rolls[i+1] == 10) {
-				score += 10 + rolls[i+2];
-				i+=2;
+			if(isSpare(frameIndex)) { //spare
+				score += 10 + rolls[frameIndex+2];
+				frameIndex+=2;
 			}else {
-				score += rolls[i] + rolls[i+1];
-				i+=2;
+				score += rolls[frameIndex] + rolls[frameIndex+1];
+				frameIndex+=2;
 			}
 		}	
 		return score;
+	}
+
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex+1] == 10;
 	}
 
 }
