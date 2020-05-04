b1a7e31c1acfd55703191077f9502f3d1d4aaf85
pc <z1top133@ajou.ac.kr>
2020-05-04 23:41:20 +0900

    The Second test.

--- eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
+++ eclipse-workspace_2/BowlingGame/test/BowlingGameTest.java
@@ -11,19 +11,21 @@ public class BowlingGameTest {
 		g = new Game();
 	}
 	
+	private void rollMany(int n, int pins) {
+		for(int i=0; i<n; i++) {
+			g.roll(pins);
+		}
+	}
+	
 	@Test
 	public void testGutterGame() {
-		for(int i=0; i<20; i++) {
-			g.roll(0);
-		}
+		rollMany(20, 0);
 		assertEquals(0, g.score());
 	}
 	
 	@Test
 	public void testAllOnes() {
-		for(int i=0; i<20; i++) {
-			g.roll(1);
-		}
+		rollMany(20,1);
 		assertEquals(20, g.score());
 	}
 
